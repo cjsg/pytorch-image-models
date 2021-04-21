98bc343e9c97e9e9d0fe776a7d632ea3914bc2b9
Carl Johann Simon-Gabriel <cjsimon@tuebingen.mpg.de>
2021-04-21 14:42:19 +0200

    [M] Saves samplers of ImageFolder parser for re-use next time

--- timm/data/parsers/parser_image_folder.py
+++ timm/data/parsers/parser_image_folder.py
@@ -6,6 +6,8 @@ on the folder hierarchy, just leaf folders by default.
 Hacked together by / Copyright 2020 Ross Wightman
 """
 import os
+import logging
+from torch import save, load
 
 from timm.utils.misc import natural_key
 
@@ -13,6 +15,8 @@ from .parser import Parser
 from .class_map import load_class_map
 from .constants import IMG_EXTENSIONS
 
+
+_logger = logging.getLogger(__name__)
 
 def find_images_and_targets(folder, types=IMG_EXTENSIONS, class_to_idx=None, leaf_name_only=True, sort=True):
     labels = []
@@ -46,12 +50,27 @@ class ParserImageFolder(Parser):
 
         self.root = root
         class_to_idx = None
-        if class_map:
-            class_to_idx = load_class_map(class_map, root)
-        self.samples, self.class_to_idx = find_images_and_targets(root, class_to_idx=class_to_idx)
-        if len(self.samples) == 0:
-            raise RuntimeError(
-                f'Found 0 images in subfolders of {root}. Supported image extensions are {", ".join(IMG_EXTENSIONS)}')
+        path_to_samples = os.path.join(root, 'samples.pth')
+        if os.path.exists(path_to_samples):
+            _logger.info(
+                f'Using pre-computed (sample, ix) files samples.pth found in {root}')
+            self.samples = load(path_to_samples)
+        else:
+            _logger.info(
+                f'No file samples.pth in folder {root}.\n'
+                f'I will reccursively traverse the folder to construct and save it.')
+            if class_map:
+                class_to_idx = load_class_map(class_map, root)
+            self.samples, self.class_to_idx = find_images_and_targets(root, class_to_idx=class_to_idx)
+            if len(self.samples) == 0:
+                raise RuntimeError(
+                    f'Found 0 images in subfolders of {root}. Supported image extensions are {", ".join(IMG_EXTENSIONS)}')
+            try:
+                save(self.samples, path_to_samples)
+            except PermissionError:
+                _logger.info(
+                    f'Could not save (sample, ix)-dictionnary, because I have no writing permissions for {root}'
+                )
 
     def __getitem__(self, index):
         path, target = self.samples[index]
