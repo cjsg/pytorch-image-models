77c6ac533dfb77e11c4983100345f91f1383bc79
Carl Johann Simon-Gabriel <cjsimon@tuebingen.mpg.de>
2021-07-09 15:54:50 +0200

    [M] added find_unused_parameters=True option to NativeDDP

--- train.py
+++ train.py
@@ -468,7 +468,7 @@ def main():
         else:
             if args.local_rank == 0:
                 _logger.info("Using native Torch DistributedDataParallel.")
-            model = NativeDDP(model, device_ids=[args.local_rank])  # can use device str in Torch >= 1.1
+            model = NativeDDP(model, device_ids=[args.local_rank], find_unused_parameters=True)  # can use device str in Torch >= 1.1
         # NOTE: EMA model does not need to be wrapped by DDP
 
     # setup learning rate schedule and starting epoch
