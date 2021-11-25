b0affae628358d8df1b1e3f71606022bf840ec9c
Carl Johann Simon-Gabriel <cjsimon@tuebingen.mpg.de>
2021-11-25 11:57:26 +0100

    [M] distributed_avtrain.sh launches with python instead of envs/torchtest/bin/python; activating conda environment manually in parent sh

--- distributed_avtrain.sh
+++ distributed_avtrain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 NUM_PROC=$1
 shift
-/home/cjsimon/anaconda3/envs/torchtest/bin/python -m torch.distributed.launch --nproc_per_node=$NUM_PROC avtrain.py "$@"
+python -m torch.distributed.launch --nproc_per_node=$NUM_PROC avtrain.py "$@"
+# /home/cjsimon/anaconda3/envs/torchtest/bin/python -m torch.distributed.launch --nproc_per_node=$NUM_PROC avtrain.py "$@"
